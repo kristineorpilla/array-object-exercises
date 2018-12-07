29c76d83441047aaca9e94985e609292c2682bda
MarkIsNotTaken <mdemesillo@gmail.com>
2018-12-07 10:38:58 -1000

    first

---
+++ exercises.js
@@ -0,0 +1,14 @@
+//1
+var myPenguin = {name: "Pablo", origin: "The Backyardigans"};
+//2
+function hello (name) {
+  return "Hello, I'm a penguin and my name is " + name + "!";
+}
+console.log(hello(myPenguin.name));
+//3
+myPenguin.canFly = "false";
+//4
+var myPenguin = {chirp: "CHIRP CHIRP! Is this what penguins sound like?"}
+console.log(myPenguin.chirp);
+//5
+var myPenguin = {}
