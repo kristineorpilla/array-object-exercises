047d8e033b2841b5cec280f4343aef194ef42b1f
MarkIsNotTaken <mdemesillo@gmail.com>
2018-12-10 09:32:40 -1000

    second

--- exercises.js
+++ exercises.js
@@ -6,12 +6,14 @@ function hello (name) {
 }
 console.log(hello(myPenguin.name));
 //3
-var myPenguin = {canFly = "false"};
+var myPenguin = {canFly: "false"};
 //4
 myPenguin.chirp = function() {
 	return "CHIRP CHIRP! Is this what penguins sound like?"
 };
 //5
-myPenguin.sayHello = function() {
-	return hello 
+myPenguin.sayHello = function(name) {
+	return "Hello, I'm a penguin and my name is " + name + "!"; 
 }
+//6
+console.log(myPenguin.sayHello(kristine));
