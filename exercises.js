3b53354f7b4c5eab6803adac41c0c5e323e92afb
Your Name <you@example.com>
2018-12-10 20:48:46 -1000

    third

--- exercises.js
+++ exercises.js
@@ -10,10 +10,23 @@ var myPenguin = {canFly: "false"};
 //4
 myPenguin.chirp = function() {
 	return "CHIRP CHIRP! Is this what penguins sound like?"
-};
+}
+console.log(myPenguin.chirp());
 //5
 myPenguin.sayHello = function(name) {
 	return "Hello, I'm a penguin and my name is " + name + "!"; 
 }
 //6
-console.log(myPenguin.sayHello(kristine));
+console.log(myPenguin.sayHello("Bob"));
+//7
+console.log(myPenguin.sayHello("Penguin McPenguinFace"));
+//8
+myPenguin.fly = function() {
+	if (this.canFly = "true") {
+		return "I can fly!";
+	}
+	else {
+		return "No flying for me!";
+	}
+}
+console.log(myPenguin.fly(this.canFly));
